30b42250c078f0cd71ae024b76c3596200770a02
Pritam Samanta <131782970+pritam704737@users.noreply.github.com>
2026-02-21 09:03:52 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,23 @@
+<style type="text/css">
+<!--
+body {
+	margin-left: 0px;
+	margin-top: 0px;
+}
+-->
+</style>
+	<?php
+	if(isset($_SESSION['login']))
+	{
+	 echo "<div align=\"right\"><strong><a href=\"index.php\"> Home </a>|<a href=\"signout.php\">Signout</a></strong></div>";
+	 }
+	 else
+	 {
+	 	echo "&nbsp;";
+	 }
+	?>
+	</td>
+	
+  </tr>
+  
+</table>
